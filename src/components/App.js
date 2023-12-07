18fdff538287b91b67911612166155a7fab94c5b
object-ions <moshikoatia@gmail.com>
2023-12-06 16:04:48 -0800

    Configure project

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./Header";
-import ToDoControl from "./ToDoControl";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <ToDoControl />
+
     </>
   );
 }
--- src/components/NewToDoForm.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-import ReusableForm from "./ReusableForm"
-
-function NewToDoForm() {
-  return <></>;
-}
-
-export default NewToDoForm;
