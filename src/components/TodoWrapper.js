36f607589c7807f7249ec473b5405696c9a831ed
object-ions <moshikoatia@gmail.com>
2023-12-07 21:20:18 -0800

    Add edit and delete fx

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -7,19 +7,45 @@ uuidv4();
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => {
-    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
     console.log(todos);
   };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodos(todos.map((todo) => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+  };
+
   return (
     <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo
+          task={todo}
+          key={index}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+        />
       ))}
-      
     </div>
-  )
+  );
 };
 
 export default TodoWrapper;
